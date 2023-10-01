bb3298e7c11467fdba7a1263ebd4815c0fa85be1
x1unix <denis0051@gmail.com>
2023-10-01 22:18:55 +0200

    feat: add cmdline tracer

--- internal/platform/cmd.go
+++ internal/platform/cmd.go
@@ -2,6 +2,7 @@ package platform
 
 import (
 	"context"
+	"os"
 	"os/exec"
 	"sync"
 
@@ -11,10 +12,24 @@ import (
 var (
 	cmdRunner CommandRunner
 	cmdOnce   sync.Once
+
+	_, isCmdDryRun = os.LookupEnv("COLIMA_TUN_DRY_RUN")
 )
 
 type CommandRunner interface {
 	RunCommand(ctx context.Context, name string, args ...string) error
+}
+
+type noopCommandRunner struct {
+	log zerolog.Logger
+}
+
+func (s noopCommandRunner) RunCommand(ctx context.Context, name string, args ...string) error {
+	s.log.Debug().
+		Str("cmd", name).
+		Strs("cmdline", append([]string{name}, args...)).
+		Msgf("running command: '%s %s'", name, args)
+	return nil
 }
 
 type systemCommandRunner struct {
@@ -22,9 +37,11 @@ type systemCommandRunner struct {
 }
 
 func (r systemCommandRunner) RunCommand(ctx context.Context, name string, args ...string) error {
+	cmd := exec.CommandContext(ctx, name, args...)
+
 	stdout := r.log.With().
 		Str("cmd", name).
-		Strs("args", args).
+		Strs("cmdline", cmd.Args).
 		Str("type", "stdout").
 		Str(zerolog.LevelFieldName, zerolog.DebugLevel.String()).
 		Logger()
@@ -32,11 +49,10 @@ func (r systemCommandRunner) RunCommand(ctx context.Context, name string, args .
 	stderr := r.log.With().
 		Str(zerolog.LevelFieldName, zerolog.ErrorLevel.String()).
 		Str("cmd", name).
-		Strs("args", args).
+		Strs("cmdline", cmd.Args).
 		Str("type", "stderr").
 		Logger()
 
-	cmd := exec.CommandContext(ctx, name, args...)
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
 	return cmd.Run()
@@ -45,6 +61,10 @@ func (r systemCommandRunner) RunCommand(ctx context.Context, name string, args .
 // GetCommandRunner returns global command runner.
 func GetCommandRunner(logger zerolog.Logger) CommandRunner {
 	cmdOnce.Do(func() {
+		if isCmdDryRun {
+			cmdRunner = noopCommandRunner{log: logger}
+			return
+		}
 		cmdRunner = systemCommandRunner{log: logger}
 	})
 
